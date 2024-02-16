213c976c5f89f99768f7afc735aeb3f264befb42
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-16 19:23:38 +0100

    FEAT: added name to program, added copydirectory function and command; added bin to package.json

--- index.js
+++ index.js
@@ -7,9 +7,34 @@
 "use strict";
 
 import { program } from "commander";
+import fs from "node:fs";
+import path from "node:path";
 
 program
+    .name("CopyDirectory")
     .version("1.0.0")
     .description("A simple CLI to copy one directory into another directory")
+
+const copyDirectory = (from, to, options) => {
+    const originalPath = path.resolve(from);
+    const newPath = path.resolve(to);
+
+    if (!options.quiet) console.log(`Copying ${originalPath} to ${newPath}`);
+    if (!fs.existsSync(originalPath))
+    {
+        if (!options.quiet) console.error(`from directory (${from}) doesn't exist!`);
+        return
+    }
+}
+
+program.command("cp")
+       .description("Copy a directory.")
+       .argument("<from>", "The directory to copy.")
+       .argument("<to>", "Where the directory will be afterwards.")
+       .option("-Q, --quiet","Don't output anything to the console.")
+       .option("-O, --OverWrite", "Overwrite existing directectory (and its contents) if target/to directory alread exists.")
+       .action(copyDirectory)
+
+program.helpCommand(true)
 
 program.parse(process.argv);
