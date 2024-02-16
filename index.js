6188618b30acc522b6f0cc2783b61948357fcd9e
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-16 20:10:50 +0100

    FEAT: fixed bin thing

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /*
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -12,7 +13,7 @@ import path from "node:path";
 
 program
     .name("cpDirectory")
-    .version("1.0.0")
+    .version("1.0.1")
 
 const copyDirectory = (from, to) => {
     const originalPath = path.resolve(from);
