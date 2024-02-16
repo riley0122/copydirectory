197f1d25f0161dce992dd04e89c45d29414113f3
Bread <77288737+riley0122@users.noreply.github.com>
2024-02-16 19:51:01 +0100

    FEAT: added bin to lock? ; made the arguements directly onto the program; make it copy over the files now

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ import path from "node:path";
 program
     .name("CopyDirectory")
     .version("1.0.0")
-    .description("A simple CLI to copy one directory into another directory")
 
-const copyDirectory = (from, to, options) => {
+const copyDirectory = (from, to) => {
     const originalPath = path.resolve(from);
     const newPath = path.resolve(to);
+
+    const options = program.opts();
 
     if (!options.quiet) console.log(`Copying ${originalPath} to ${newPath}`);
     if (!fs.existsSync(originalPath))
@@ -25,14 +26,30 @@ const copyDirectory = (from, to, options) => {
         if (!options.quiet) console.error(`from directory (${from}) doesn't exist!`);
         return
     }
+
+    if (!fs.existsSync(newPath)) fs.mkdirSync(newPath);
+
+    fs.readdirSync(originalPath, {withFileTypes: true}).forEach(item => {
+        if (item.isDirectory())
+        {
+            if (options.verbose) console.log(`${item.name} is a directory`);
+            copyDirectory(path.join(originalPath, item.name), path.join(newPath, item.name));
+        } else
+        {
+            if (options.verbose) console.log(`copying ${item.name}...`);
+
+            fs.copyFileSync(path.join(originalPath, item.name), path.join(newPath, item.name));
+        }
+    });
 }
 
-program.command("cp")
+program
        .description("Copy a directory.")
        .argument("<from>", "The directory to copy.")
        .argument("<to>", "Where the directory will be afterwards.")
        .option("-Q, --quiet","Don't output anything to the console.")
        .option("-O, --OverWrite", "Overwrite existing directectory (and its contents) if target/to directory alread exists.")
+       .option("--verbose", "Log things to the console that otherwise wouldn't be logged.")
        .action(copyDirectory)
 
 program.helpCommand(true)
